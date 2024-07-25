5fdc865104c455a6faa5eaba22cb583b54aeff24
dhruvatgithub2004 <118565242+dhruvatgithub2004@users.noreply.github.com>
2024-07-25 20:15:04 +0530

    Update purchase_prediction_app.py

--- purchase_prediction_app.py
+++ purchase_prediction_app.py
@@ -21,8 +21,8 @@ def main():
     gender = st.selectbox('Gender', options=[0, 1], format_func=lambda x: 'Male' if x == 0 else 'Female')
     annual_income = st.number_input('Annual Income', min_value=0, value=50000)
     number_of_purchases = st.number_input('Number of Purchases', min_value=0, value=1)
-    product_category = st.selectbox('Product Category', options=[0, 1, 2, 3, 4], format_func=lambda x:
-                                    ['Electronics', 'Clothing', 'Home Goods', 'Beauty', 'Sports'][x])
+    # product_category = st.selectbox('Product Category', options=[0, 1, 2, 3, 4], format_func=lambda x:
+    #                                 ['Electronics', 'Clothing', 'Home Goods', 'Beauty', 'Sports'][x])
     time_spent_on_website = st.number_input('Time Spent on Website (minutes)', min_value=0, value=10)
     loyalty_program = st.selectbox('Loyalty Program', options=[0, 1], format_func=lambda x: 'Yes' if x == 1 else 'No')
     discounts_availed = st.number_input('Discounts Availed', min_value=0, max_value=5, value=0)
